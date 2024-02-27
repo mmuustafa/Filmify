41813e81c32664485598a0ff6a92ae64222bbfb4
jashanbir1 <jmann028@ucr.edu>
2024-02-26 18:23:19 -0800

    adding MovieMethods_header

--- header/MovieMethods.h
+++ header/MovieMethods.h
@@ -5,7 +5,30 @@
 
 using namespace std;
 
-class Movies {
+class MovieMethods {
+
+    private:
+    vector<Movie> reccomendedMovies;
+    vector<Movie> everyMovieEver
+
+    public:
+    MovieMethods();
+    Movie GetMovie(string );
+    void SortByAlphabetical();
+    void SortByRating();
+    void SortByYear();
+    void PrintAllGenres();
+    void PrintFinalListOfReccs();
+    void CreateReccs(int ); //will create reccs based on actor, genre, director. Will call the helper functions based on Int input for
+    //1 is movie name, 2 is actor, 3 is genre, 4 is director
+
+    private:
+    void CreateReccsBasedOnMovieName(string );
+    void CreateReccsBasedOnActor(string );
+    void CreateReccsBasedOnGenre(string );
+    void CreateReccsBasedOnDirector(string );
+
+
 };
 
 
