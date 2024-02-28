e7748ad7e6608f469e6ba0c874e017baee57583d
jashanbir1 <jmann028@ucr.edu>
2024-02-27 21:54:18 -0800

    worked on moviedataset_cpp

--- src/MovieDataSet.cpp
+++ src/MovieDataSet.cpp
@@ -4,10 +4,40 @@
 #include<fstream>
 #include<sstream>
 #include<cassert>
+#include<vector>
 
 using namespace std;
 
 vector<Movie> MovieDataSet:: generateListOfMovies()
 {
-    
+    vector<Movie> fillMovies;
+    ifstream infs("../lib/movies.csv");
+
+    if (!infs.is_open())
+    {
+        cout << "Could not open file!" << endl;
+        return fillMovies; //return empty vector
+    }
+
+    string spaceLine;
+    while (getline(infs,spaceLine))
+    {
+        stringstream iss(spaceLine);
+        Movie movieObj;
+
+        getline(iss, movieObj.movieName, ',');
+        getline(iss, movieObj.actor, ',');
+        getline(iss, movieObj.genre, ',');
+        getline(iss, movieObj.director, ',');
+        getline(iss, movieObj.rating, ',');
+        iss >> movieObj.year;
+
+        iss.ignore();
+
+        fillMovies.push_back(movieObj);
+
+    }
+
+    infs.close();
+    return fillMovies;
 }
